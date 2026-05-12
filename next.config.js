ca85dfd7cc1f30cfc68237b81f29a5b87f980efe
DorianNoam <dorian33270@hotmail.fr>
2026-05-12 12:19:47 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
+            // LE FIX EST ICI : on autorise 'unsafe-eval'
             value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.supabase.co https://js.stripe.com; object-src 'none';",
           },
         ],
