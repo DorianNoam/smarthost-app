2a9e0e2b199c51425a60ec8a2da3fc77efa528ba
DorianNoam <dorian33270@hotmail.fr>
2026-05-16 19:24:06 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import Script from 'next/script';
-import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* Google Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-Z2KKW1DB52"
         strategy="afterInteractive"
