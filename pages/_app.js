81623b79c5b970926cf3a64ac0f0d1072b130b1d
DorianNoam <dorian33270@hotmail.fr>
2026-05-18 09:02:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-Z2KKW1DB52"
         strategy="afterInteractive"
