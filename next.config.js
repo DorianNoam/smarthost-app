62b177c53404c4f335c2c3ea8d22f1cf6f5bb807
DorianNoam <dorian33270@hotmail.fr>
2026-05-24 15:09:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // ── Internationalisation ──────────────────────────────────
-  i18n: {
-    locales: ['fr', 'en', 'es'],
-    defaultLocale: 'fr',
-  },
-
   async headers() {
     return [
       {
